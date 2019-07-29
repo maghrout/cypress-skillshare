719cd311fbaf15d7520394066efa2f6ba3801067
Moujib Aghrout <maghrout@googlemail.com>
2019-07-29 16:23:35 +0100

    Feat: Add in new tests for visiting certain pages

---
+++ cypress/integration/example_spec.js
@@ -0,0 +1,23 @@
+describe("My First Passing Test", function() {
+  it("Does not do much!", function() {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("My First Failing Test", function() {
+  it("Does not do much!", function() {
+    expect(true).to.equal(false);
+  });
+});
+
+describe("My First Example Test", function() {
+  it("Visits the Kitchen Sink", function() {
+    cy.visit("https://example.cypress.io");
+  });
+});
+
+describe("My First Hillary's Test", function() {
+  it("Visits the Hillary's ite", function() {
+    cy.visit("https://example.cypress.io");
+  });
+});
