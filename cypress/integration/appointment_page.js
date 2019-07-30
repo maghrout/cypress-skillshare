3f49d238417dea93c92a584b1db3647e156a8892
Moj <maghrout@googlemail.com>
2019-07-30 13:25:34 +0100

    Feat: make second passing test and update gitignore and cypress config

--- cypress/integration/appointment_page.js
+++ cypress/integration/appointment_page.js
@@ -1,8 +1,6 @@
 describe("Hillarys Appointment Page Test", function() {
-  it("", function() {
-    cy.visit(
-      "https://www.dev.hillarys.hblonline.co.uk/arrange-an-appointment/"
-    );
+  it("Successfully completes form", function() {
+    cy.visit("/arrange-an-appointment/");
 
     //Step 1
     cy.get('[data-products="Blinds"]').click();
@@ -66,5 +64,52 @@ describe("Hillarys Appointment Page Test", function() {
       "eq",
       "https://www.dev.hillarys.hblonline.co.uk/arrange-an-appointment/confirmation/"
     );
+  });
+
+  it("Validation message should appear if second date is not picked", function() {
+    cy.visit("/arrange-an-appointment/");
+
+    //Step 1
+    cy.get('[data-products="Blinds"]').click();
+    cy.get('[data-products="Curtains"]').click();
+    cy.get('[data-products="Conservatory-roof-blinds"]').click();
+
+    cy.get("[data-next-section]")
+      .eq(0)
+      .click();
+
+    //Step 2
+    cy.get("[data-postcode-enter-manually]").click();
+
+    cy.get("#buildingNumber").type("7");
+    cy.get("#addressline1").type("Dale Street");
+    cy.get("#addressline3").type("Lancashire");
+    cy.get("#town").type("Manchester");
+    cy.get("#postcode").type("M1 1JA");
+
+    cy.get("[data-next-section]")
+      .eq(1)
+      .click();
+
+    //Step 3
+    //Normally you can only select an option from a dropdown using it's value
+    //This is a nice work around for picking a random number from the dropdown not based on value
+    cy.get("#selecteddate1 > option")
+      .eq(3)
+      .then(element => cy.get("#selecteddate1").select(element.val()));
+    cy.get("#selectedtime1").select("09:00 - 12:00");
+
+    //Uncomment to make test fail
+    // cy.get("#selecteddate2 > option")
+    //   .eq(4)
+    //   .then(element => cy.get("#selecteddate2").select(element.val()));
+    // cy.get("#selectedtime2").select("09:00 - 12:00");
+
+    cy.get("[data-next-section]")
+      .eq(2)
+      .click();
+
+    // //Confirmation
+    cy.get("[data-appointment-times-error-message]").should("be.visible");
   });
 });
