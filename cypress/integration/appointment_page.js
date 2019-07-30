94e6088df82b94f5d2036f1674684ef2c278318f
Moujib Aghrout <maghrout@googlemail.com>
2019-07-30 10:58:00 +0100

    Feat: Create step 3 of test with some nice logic for dropdown

--- cypress/integration/appointment_page.js
+++ cypress/integration/appointment_page.js
@@ -23,5 +23,21 @@ describe("Hillarys Appointment Page Test", function() {
     cy.get("[data-next-section]")
       .eq(1)
       .click();
+
+    //Step 3
+    cy.get("#selecteddate1 > option")
+      .eq(3)
+      .then(element => cy.get("#selecteddate1").select(element.val()));
+    cy.get("#selectedtime1").select("09:00 - 12:00");
+
+    cy.get("#selecteddate2 > option")
+      .eq(4)
+      .then(element => cy.get("#selecteddate2").select(element.val()));
+    cy.get("#selectedtime2").select("09:00 - 12:00");
+
+    cy.get("#selecteddate3 > option")
+      .eq(5)
+      .then(element => cy.get("#selecteddate3").select(element.val()));
+    cy.get("#selectedtime3").select("09:00 - 12:00");
   });
 });
