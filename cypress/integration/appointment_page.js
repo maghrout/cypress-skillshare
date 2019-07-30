76a85d75b48d1f02f518ffa184c55f4515502380
Moujib Aghrout <maghrout@googlemail.com>
2019-07-30 10:57:26 +0100

    Feat: Create step 2 of test

--- cypress/integration/appointment_page.js
+++ cypress/integration/appointment_page.js
@@ -10,5 +10,18 @@ describe("Hillarys Appointment Page Test", function() {
     cy.get("[data-next-section]")
       .eq(0)
       .click();
+
+    //Step 2
+    cy.get("[data-postcode-enter-manually]").click();
+
+    cy.get("#buildingNumber").type("7");
+    cy.get("#addressline1").type("Dale Street");
+    cy.get("#addressline3").type("Lancashire");
+    cy.get("#town").type("Manchester");
+    cy.get("#postcode").type("M1 1JA");
+
+    cy.get("[data-next-section]")
+      .eq(1)
+      .click();
   });
 });
