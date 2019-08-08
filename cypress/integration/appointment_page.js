950428760ef898b7f74aa9134e862180a3d1eefd
Moj <maghrout@googlemail.com>
2019-08-08 15:57:56 +0100

    Feat: Push new tests for ireland site

--- cypress/integration/appointment_page.js
+++ cypress/integration/appointment_page.js
@@ -73,6 +73,69 @@ describe("Hillarys Appointment Page Test", function() {
     });
   });
 
+  it(`Successfully completes form on IE site`, function() {
+    cy.visitIE("/arrange-an-appointment/");
+
+    //Step 1
+    cy.get('[data-products="Blinds"]').click();
+    cy.get('[data-products="Conservatory-roof-blinds"]').click();
+
+    cy.get("[data-next-section]")
+      .eq(0)
+      .click();
+
+    //Step 2
+    cy.get("#buildingNumber").type("7");
+    cy.get("#addressline1").type("Dale Street");
+    cy.get("#addressline3").type("Lancashire");
+    cy.get("#town").type("Manchester");
+
+    cy.get("[data-next-section]")
+      .eq(1)
+      .click();
+
+    //Step 3
+    //Normally you can only select an option from a dropdown using it's value
+    //This is a nice work around for picking a random number from the dropdown not based on value
+    cy.get("#selecteddate1 > option")
+      .eq(3)
+      .then(element => cy.get("#selecteddate1").select(element.val()));
+    cy.get("#selectedtime1").select("09:00 - 12:00");
+
+    cy.get("#selecteddate2 > option")
+      .eq(4)
+      .then(element => cy.get("#selecteddate2").select(element.val()));
+    cy.get("#selectedtime2").select("09:00 - 12:00");
+
+    cy.get("#selecteddate3 > option")
+      .eq(5)
+      .then(element => cy.get("#selecteddate3").select(element.val()));
+    cy.get("#selectedtime3").select("09:00 - 12:00");
+
+    cy.get("[data-next-section]")
+      .eq(2)
+      .click();
+
+    //Step 4
+
+    cy.get("#mr").click();
+    cy.get("#firstName").type("Testing");
+    cy.get("#surname").type("Tester");
+    cy.get("#emailAddress").type("apache@codecomputerlove.com");
+    cy.get("#ConfirmEmailAddress").type("apache@codecomputerlove.com");
+    cy.get("#telephone").type("0209168046");
+
+    cy.get("[data-form-submit]")
+      .eq(0)
+      .click();
+
+    //Confirmation
+    cy.url().should(
+      "eq",
+      "https://www.dev.hillarys.ie.hblonline.co.uk/arrange-an-appointment/confirmation/"
+    );
+  });
+
   it("Validation message should appear if second date is not picked", function() {
     cy.visit("/arrange-an-appointment/");
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("visitIE", url => {
+  cy.visit(Cypress.env("ieUrl") + url);
+});
